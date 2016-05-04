de2448917419f7d82480b3985de150d0a806f2f5
ptah101 <pmugisha@gmail.com>
2016-05-04 08:38:48 +0200

    TwitRepository - Implemented GetAll and GetByFilter

--- Twitable.FileRepository/TwitRepository.cs
+++ Twitable.FileRepository/TwitRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Twitable.EntityManager;
 using Twitable.EntityManager.Filter;
@@ -8,18 +10,45 @@ namespace Twitable.FileRepository
 {
     public class TwitRepository:ITwitRepository
     {
-
         private readonly string _twitFilePath;
 
         public TwitRepository(string filePath)
         {
             _twitFilePath = filePath;
         }
+
+        /// <summary>
+        /// Retrieves all twits from the specified file path into a list of Twit objects
+        /// </summary>
+        /// <returns>The list of twit objects.</returns>
         public IQueryable<Twit> GetAll()
         {
-            throw new NotImplementedException();
+            var twitList = new List<Twit>();
+            using (var reader = new StreamReader(_twitFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bool isValid = line.Contains(">") && line.Split('>').Length <= 140;
+                    if (isValid)
+                    {
+                        var parts = line.Split('>');
+                        var username = parts[0].Trim();
+                        var text = parts[1].Trim();
+                        twitList.Add(new Twit
+                        {
+                            UserName = username,Text = text
+                        });
+                    }
+                    else
+                    {
+                        throw new Exception("The file provided, does not contain valid twits.");
+                    }
+                }
+            }
+            return twitList.AsQueryable();
         }
-
+         
         public IQueryable<Twit> GetByFilter(TwitFilter filter)
         {
             var query = GetAll();
